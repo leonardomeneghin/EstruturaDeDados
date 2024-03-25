3fffba9a76c5f1acd2d08bd3ea3f80de0256e388
Leo <leonardozenni@gmail.com>
2024-03-25 16:26:30 -0300

    feat: considering exception options to string-chain cammelCase feat: added Regex to find SpecialChars test: add test to handle more than three words test: add test to handle number and special chars

--- EstruturaDeDados/Anticorruption/StringHandler/CamelCaseHandler.cs
+++ EstruturaDeDados/Anticorruption/StringHandler/CamelCaseHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EstruturaDeDados.Anticorruption.StringHandler
@@ -22,6 +23,7 @@ namespace EstruturaDeDados.Anticorruption.StringHandler
         }
         public List<string> ConverterCamelCase(string palavra)
         {
+            ValidateStringChain(palavra);
             arrayPalavra = palavra.ToCharArray();
             LowerFirstAndLastChar(arrayPalavra);
             SplitWords(arrayPalavra);
@@ -36,17 +38,44 @@ namespace EstruturaDeDados.Anticorruption.StringHandler
             return _listaPalavras;
         }
 
+        private void ValidateStringChain(string palavra)
+        {
+            Regex specialChars = new Regex("[^A-Za-z0-9,]");
+            if(specialChars.Match(palavra).Success)
+                throw new InvalidOperationException("Cannot operate with special characters");
+            int numParsed = 0;
+            if (Int32.TryParse(palavra[0].ToString(), out numParsed))
+                throw new InvalidOperationException("String-chain cannot begins with number");
+        }
+
         public void LowerFirstAndLastChar(char[] palavra)
         {
             palavra[0] = char.ToLower(palavra[0]);
             palavra[palavra.Length -1] = char.ToLower(palavra[palavra.Length - 1]);
         }
+        private bool VerifyNumberStringOrder(char[] arrayPalavra, int i)
+        {
+            int numParse = 0;
+            bool isNumber = Int32.TryParse(arrayPalavra[i].ToString(), out numParse);
+            if (i == 0)
+            {
+                return false;
+            }
+            //Actual is Number and previous was lower char
+            if (isNumber && char.IsLower(arrayPalavra[i-1]))
+                return true;
+            //Actual is UpperChar and previos was number
+            if (char.IsUpper(arrayPalavra[i]) && Int32.TryParse(arrayPalavra[i-1].ToString(), out numParse))
+                return true;
+            return false;
+        }
+
         private void SplitWords(char[] arrayPalavra)
         {
-            
             for (int i = 0; i < arrayPalavra.Length; i++)
             {
-                if (char.IsUpper(arrayPalavra[i]) && char.IsLower(arrayPalavra[i - 1]))
+                if (char.IsUpper(arrayPalavra[i]) && char.IsLower(arrayPalavra[i - 1])
+                    || VerifyNumberStringOrder(arrayPalavra, i))
                     cadeiaPalavras+= ",";
                 cadeiaPalavras += (arrayPalavra[i].ToString());
             }
--- EstruturaDeDadosTest/AntiCorruption/StringHandler/CamelCaseHandlerTest.cs
+++ EstruturaDeDadosTest/AntiCorruption/StringHandler/CamelCaseHandlerTest.cs
@@ -1,4 +1,5 @@
-﻿using EstruturaDeDados.Anticorruption.StringHandler;
+﻿using EstruturaDeDados.Anticorruption.DataStructures.Pilha;
+using EstruturaDeDados.Anticorruption.StringHandler;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -67,6 +68,62 @@ namespace EstruturaDeDadosTest.AntiCorruption.StringHandler
             Assert.That(listaPalavrasConvertidas.Count, Is.EqualTo(2));
             Assert.That(listaPalavrasConvertidas.Contains(word1), Is.EqualTo(true));
             Assert.That(listaPalavrasConvertidas.Contains(word2), Is.EqualTo(true));
+        }
+        [TestCase]
+        public void ThreeWordWithCPF()
+        {
+            string palavra = "nomeSobrenomeCompostoCPF";
+            var listaPalavrasConvertidas = _handler.ConverterCamelCase(palavra);
+            string word1 = palavra.Substring(0, 4).ToLower();
+            string word2 = palavra.Substring(4, 9).ToLower();
+            string word3 = palavra.Substring(13, 8).ToLower();
+            string word4 = palavra.Substring(21, 3).ToLower();
+
+
+            Assert.That(listaPalavrasConvertidas.Count, Is.EqualTo(4));
+            Assert.That(listaPalavrasConvertidas.Contains(word1), Is.EqualTo(true));
+            Assert.That(listaPalavrasConvertidas.Contains(word2), Is.EqualTo(true));
+            Assert.That(listaPalavrasConvertidas.Contains(word3), Is.EqualTo(true));
+            Assert.That(listaPalavrasConvertidas.Contains(word4), Is.EqualTo(true));
+        }
+        [TestCase]
+        public void TwoWordsAndNumber()
+        {
+            string palavra = "nome10";
+            var listaPalavrasConvertidas = _handler.ConverterCamelCase(palavra);
+            string word1 = palavra.Substring(0, 4).ToLower();
+            string word2Number = palavra.Substring(palavra.Length - 2, 2).ToLower();
+
+
+
+            Assert.That(listaPalavrasConvertidas.Count, Is.EqualTo(2));
+            Assert.That(listaPalavrasConvertidas.Contains(word1), Is.EqualTo(true));
+            Assert.That(listaPalavrasConvertidas.Contains(word2Number), Is.EqualTo(true));
+
+        }
+        /// <summary>
+        /// Must return a InvalidOperationException()
+        /// </summary>
+        [TestCase]
+        public void StartWithNumberException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                string palavra = "10Nome";
+                var listaPalavrasConvertidas = _handler.ConverterCamelCase(palavra);
+            });
+        }
+        /// <summary>
+        /// Must return a InvalidOperationException()
+        /// </summary>
+        [TestCase]
+        public void SpecialCharactersException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                string palavra = "Nome%Sobrenome";
+                var listaPalavrasConvertidas = _handler.ConverterCamelCase(palavra);
+            });
         }
     }
 }
