39c91645992cb74f20e83b1260df70052f4c8d33
Leo <leonardozenni@gmail.com>
2024-03-26 20:13:17 -0300

    refactor: adicionando checklist dos testes e refactor da classe tradutor

---
+++ EstruturaDeDados/Anticorruption/Tradução/Tradutor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDeDados.Anticorruption.Tradução
+{
+    public class Tradutor
+    {
+        public IDictionary<string, string> storedTraductions { get; set; }
+        public Tradutor()
+        {
+            storedTraductions = new Dictionary<string, string>();
+        }
+        public void AddTraduction(string key, string value)
+        {
+            storedTraductions.Add(key, value);
+        }
+
+        public bool IsEmpty()
+        {
+            return storedTraductions.Count == 0;
+        }
+
+        public string Traduzir(string word)
+        {
+            return storedTraductions.Where(x => x.Key.Equals(word)).Select(x => x.Value).FirstOrDefault();
+        }
+    }
+}
---
+++ EstruturaDeDadosTest/AntiCorruption/Tradução/TradutorTest.cs
@@ -0,0 +1,47 @@
+﻿using EstruturaDeDados.Anticorruption.Tradução;
+namespace EstruturaDeDadosTest.AntiCorruption.Tradução
+{
+    public class TradutorTest
+    {
+        private Tradutor _tradutor;
+        [SetUp]
+        public void SetUp()
+        {
+            _tradutor = new Tradutor();
+        }
+        [Test]
+        public void TraductorIsEmpty()
+        {
+            Assert.That(_tradutor.IsEmpty(), Is.EqualTo(true));
+        }
+        [Test]
+        public void TraductOneTimeGood()
+        {
+            _tradutor.AddTraduction("bom", "good");
+
+
+            Assert.That(_tradutor.Traduzir("bom"), Is.EqualTo("good"));
+            Assert.That(_tradutor.IsEmpty(), Is.EqualTo(false));
+        }
+        [Test]
+        public void TraductOneTimeBad()
+        {
+            _tradutor.AddTraduction("ruim", "bad");
+
+
+            Assert.That(_tradutor.Traduzir("ruim"), Is.EqualTo("bad"));
+            Assert.That(_tradutor.IsEmpty(), Is.EqualTo(false));
+        }
+        [Test]
+        public void TraductTwoWords()
+        {
+            _tradutor.AddTraduction("ruim", "bad");
+            _tradutor.AddTraduction("bom", "good");
+
+
+            Assert.That(_tradutor.Traduzir("ruim"), Is.EqualTo("bad"));
+            Assert.That(_tradutor.Traduzir("bom"), Is.EqualTo("good"));
+            Assert.That(_tradutor.IsEmpty(), Is.EqualTo(false));
+        }
+    }
+}
