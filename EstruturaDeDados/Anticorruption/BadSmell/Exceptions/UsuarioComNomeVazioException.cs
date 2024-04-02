7112836f11ae1df5c5c5b37e3742efefe8cd63e2
Leo <leonardozenni@gmail.com>
2024-04-02 09:52:25 -0300

    feat: exercicio de fixação para encontrar e resolver code smells

---
+++ EstruturaDeDados/Anticorruption/BadSmell/Exceptions/UsuarioComNomeVazioException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDeDados.Anticorruption.BadSmell.Exceptions
+{
+    public class UsuarioComNomeVazioException : Exception
+    {
+        public UsuarioComNomeVazioException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ EstruturaDeDados/Anticorruption/BadSmell/RegistraUsuario.cs
@@ -0,0 +1,61 @@
+﻿using EstruturaDeDados.Anticorruption.BadSmell.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDeDados.Anticorruption.BadSmell
+{
+    public class RegistraUsuario
+    {
+        private List<Usuario> _usuarios;
+        RegistraUsuario()
+        {
+            _usuarios = new List<Usuario>();
+        }
+        public void OLDregistraUsuarios(string nome)
+        {
+            try
+            {
+                if (nome != null)
+                {
+                    if(!string.IsNullOrEmpty(nome))
+                    {
+                        Usuario usuario = new Usuario(nome);
+                        if (!_usuarios.Contains(usuario))
+                        {
+                            _usuarios.Add(usuario);
+                        }
+                        else
+                        {
+                            throw new UsuarioJaRegistradoException($"Já existe um usuário registrado com o nome {nome}, por favor utilize outro nome de usuário.");
+                        }
+                    }
+                    else
+                    {
+                        throw new UsuarioComNomeVazioException("O nome está vazio, por favor insira um nome válido");
+                    }
+                }
+                else
+                {
+                    throw new UsuarioInexistenteException("Não pode registrar usuário inexistente");
+                }
+            }
+            catch (UsuarioComNomeVazioException ex)
+            {
+                throw new UsuarioComNomeVazioException(ex.Message);
+            }
+            catch (UsuarioInexistenteException ex)
+            {
+                throw new UsuarioInexistenteException(ex.Message);
+            }
+            catch (UsuarioJaRegistradoException ex)
+            {
+                throw new UsuarioJaRegistradoException(ex.Message);
+            }
+
+            
+        }
+    }
+}
